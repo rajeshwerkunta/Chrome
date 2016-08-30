d5387767988a9feb0d0f2138dd190915df0c0577
rkunta <rkunta@CSCINDAE753191.applabs.com>
2016-08-30 17:31:10 +0530

    Desired

--- MavenProject/src/com/sample/MavenClass.java
+++ MavenProject/src/com/sample/MavenClass.java
@@ -3,6 +3,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeTest;
 
 
@@ -12,7 +14,12 @@ public class MavenClass {
 	public void sample() {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.driver.chrome", "D:\\FrameWork\\MavenProject\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--test-type");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        WebDriver driver = new ChromeDriver(capabilities);
+		//WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.in/");
